0ccf9f08a8f0a20c8eefaf98ec44df3440d9789e
Nick <raynor1@gmail.com>
2018-10-11 13:51:18 -0500

    added another variable

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static Func<int, int, int, int> add = (a, b, c) => a + b + c;
 
         int a = add(1, 2, 3);
+        int b1 = add(1, 2, 3);
 
         static int b = 1;
         static int c = 2;
